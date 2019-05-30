7eb0487f94dcebbef28839726628ba29971c6c81
Cooper Kelley <kelleyco2@gmail.com>
2019-05-29 19:21:14 -0600

    routing

---
+++ src/components/FindALeague/CreateATeam.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateATeam() {
+  return <div>Create A Team</div>;
+}
--- src/components/Navbar/Navebar.js
+++ src/components/Navbar/Navebar.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-
-
-export default class Navbar extends Component{
-    
-    render(){
-    return( 
-    <div>  
-       <h1>Navbar</h1>
-    </div>    
-    )
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Navebar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/SignUp">Sign Up</Link>
+      <Link to="/FindALeague">Find A League</Link>
+      <Link to="/CreateATeam">Create A Team</Link>
+      <Link to="/Leagues">Leagues</Link>
+    </div>
+  );
 }
-} 
